7cac9ff27fbd4c20909e4dac54d12786ab0e3482
Renaud <renaudboulay9@gmail.com>
2022-05-28 21:13:01 +0200

    indec

--- Connexion.php
+++ Connexion.php
@@ -38,11 +38,40 @@ if(sizeof($_POST) == 3) {
 ?>
 
 <html>
+<link rel="stylesheet" href="stylesheet.css">
     <head>
         <title>Projet Site web</title>
         <meta charset="UTF-8">
     </head>
     <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="navbar-brand" href="index.html">Accueil</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="animaux.html">Animaux <span class="sr-only"></span></a>
+                    </li>
+                    <li>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="ticket.html">Ticket</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="navbar-brand" href="Inscription.php">Connexion</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </header>
         <h1>Connexion</h1>
         <?php if(!isset($_SESSION['pseudo'])) {?>
         <form method="post" id="connexionForm" action="#">
